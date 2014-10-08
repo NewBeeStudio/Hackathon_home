5adfa174a4b633eac66fad59f68dd0fe9901b9c0
Honlan <493722771@qq.com>
2014-10-08 15:42:22 +0800

    data add

--- App/Lib/Action/Index/JoinAction.class.php
+++ App/Lib/Action/Index/JoinAction.class.php
@@ -6,7 +6,17 @@ class JoinAction extends Action{
 
 	public function handle(){
 		if(IS_POST){
-			p($_POST);	
+			$user = array(
+				'name' => I('name'),
+				'sex' => I('sex'),
+				'college' => I('college'),
+				'grade' => I('grade'),
+				'major' => I('major'),
+				'tel' => I('tel'),
+				'e-mail' => I('e-mail')
+				);
+			M('user')->data($user)->add();
+
 		}
 		else{
 			$this->redirect('Index/Join/index');
